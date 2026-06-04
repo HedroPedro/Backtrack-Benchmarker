a2f66573c7748d3a5d4444e4e9cfd1db0751ba87
HedroPedro <pedrohenrique.oliveira119@gmail.com>
2026-06-04 13:45:02 -0300

    fix: ns instead of seconds

--- generate_benchmark.py
+++ generate_benchmark.py
@@ -4,24 +4,25 @@ import time
 import statistics
 
 if __name__ == "__main__":
-	size = 7
-	max_size = size**2+3
+	size = 5
+	max_size = 103
 	n = 50
-	csv = open("gen_mazes.csv", "w")
-	csv.write("size;mean_time;sd\n")
+	csv_gen = open("gen_mazes.csv", "w")
+	csv_gen.write("size;mean_time;sd\n")
 	while size < max_size:
 		maze_str = f"maze_{size}x{size}"
 		gen_time = [None] * n
-		if not Path.exists(maze_str):
-			Path(maze_str).mkdir()
+		path = Path(maze_str)
+		if not path.exists():
+			path.mkdir()
 		for i in range(n):
 			with open(f"{maze_str}/{i}.maze", "w") as f:
-				start = time.perf_counter()
+				start = time.perf_counter_ns()
 				run(["./bin/generate_maze", str(size), str(size)], stdout=f)
-				end = time.perf_counter() - start
+				end = time.perf_counter_ns() - start
 				gen_time[i] = end
 		mean = statistics.mean(gen_time)
 		sd = statistics.pstdev(gen_time)
-		csv.write(f"{size**2};{mean};{sd}\n")
+		csv_gen.write(f"{size**2};{mean};{sd}\n")
 		size += 2
-	csv.close()
+	csv_gen.close()
