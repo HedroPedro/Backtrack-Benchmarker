dfafc9b6e2cccf63c21514a433ebf937cced5e07
HedroPedro <pedrohenrique.oliveira119@gmail.com>
2026-06-04 14:42:24 -0300

    fix: from 7x7 to 103x103 to benchmark

--- generate_benchmark.py
+++ generate_benchmark.py
@@ -4,25 +4,25 @@ import time
 import statistics
 
 if __name__ == "__main__":
-	size = 5
-	max_size = 103
+	size = 7
+	max_size = 104
 	n = 50
-	csv_gen = open("gen_mazes.csv", "w")
-	csv_gen.write("size;mean_time;sd\n")
+	csv = open("gen_mazes.csv", "w")
+	csv.write("size;mean_time;sd\n")
 	while size < max_size:
 		maze_str = f"maze_{size}x{size}"
+		variance = 0
 		gen_time = [None] * n
-		path = Path(maze_str)
-		if not path.exists():
-			path.mkdir()
+		if not Path(maze_str).exists():
+			Path(maze_str).mkdir()
 		for i in range(n):
 			with open(f"{maze_str}/{i}.maze", "w") as f:
-				start = time.perf_counter_ns()
+				start = time.perf_counter()
 				run(["./bin/generate_maze", str(size), str(size)], stdout=f)
-				end = time.perf_counter_ns() - start
+				end = (time.perf_counter() - start) / 100000000
 				gen_time[i] = end
 		mean = statistics.mean(gen_time)
 		sd = statistics.pstdev(gen_time)
-		csv_gen.write(f"{size**2};{mean};{sd}\n")
+		csv.write(f"{size**2};{mean};{sd}\n")
 		size += 2
-	csv_gen.close()
+	csv.close()
